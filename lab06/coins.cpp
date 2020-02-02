dc66f7e12ecbd5a4ecd0392b4faaf183b2ef77e9
Alex Lin <alexlin247@gmail.com>
2020-02-02 17:57:06 -0500

    Add simple coin detection

--- lab06/coins.cpp
+++ lab06/coins.cpp
@@ -14,18 +14,51 @@ int main(int argc, char** argv) {
     }
     Mat gray;
     cvtColor(src, gray, COLOR_BGR2GRAY);
-    GaussianBlur(gray, gray, Size(5, 5), 5);
+    GaussianBlur(gray, gray, Size(5, 5), 20);
 
     vector<Vec3f> circles;
-    HoughCircles(gray, circles, HOUGH_GRADIENT, 1, gray.cols / 50, 100, 30, gray.cols / 80,
+    HoughCircles(gray, circles, HOUGH_GRADIENT, 1, gray.cols / 80, 100, 30, gray.cols / 80,
                  gray.cols / 16);
-    for (const Vec3i& c : circles) {
-        Point center = Point(c[0], c[1]);
-        circle(src, center, 1, Scalar(0, 100, 100), 3, LINE_AA);
-        circle(src, center, c[2], Scalar(255, 0, 255), 3, LINE_AA);
+
+    Mat radii(circles.size(), 1, CV_32F), coin_labels, centers;
+    for (int i = 0; i < circles.size(); i++) {
+        radii.at<float>(i) = circles[i][2];
     }
+    kmeans(radii, 3, coin_labels,
+           TermCriteria(TermCriteria::MAX_ITER | TermCriteria::EPS, 10, .01), 10,
+           KMEANS_PP_CENTERS, centers);
+
+    for (int i = 0; i < coin_labels.rows; i++) {
+        cout << coin_labels.at<int>(i) << endl;
+    }
+    float total = 0.0f;
+    for (int i = 0; i < circles.size(); i++) {
+        Point center = Point(circles[i][0], circles[i][1]);
+        circle(src, center, 1, Scalar(0, 100, 100), 3, LINE_AA);
+        Scalar clr;
+        switch (coin_labels.at<int>(i)) {
+            case 0:
+                clr = Scalar(0, 0, 255);
+                total += 0.01f;
+                break;
+            case 1:
+                clr = Scalar(0, 255, 255);
+                total += 0.05f;
+                break;
+            case 2:
+                clr = Scalar(255, 0, 255);
+                total += 0.25f;
+                break;
+            case 3:
+                clr = Scalar(0, 0, 0);
+                total += 0.50f;
+        }
+        printf("Total: %.2f\n", total);
+        circle(src, center, circles[i][2], clr, 3, LINE_AA);
+    }
+    printf("Total: %.2f", total);
     // Show results
-	imshow("Canny Circles", gray);
+    imshow("Canny Circles", gray);
     imshow("Detected Circles", src);
     // Wait and Exit
     waitKey();
