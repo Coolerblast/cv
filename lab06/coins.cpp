42f6a0fcde7f34c2a1ce872b086ca7a365831b4c
Alex Lin <alexlin247@gmail.com>
2020-03-15 16:38:18 -0400

    Finish platonic solids lab

--- lab06/coins.cpp
+++ lab06/coins.cpp
@@ -1,3 +1,6 @@
+// Name: Alex Lin
+// Date: 02/23/2020
+
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
@@ -12,53 +15,137 @@ int main(int argc, char** argv) {
         printf(" Program Arguments: [image_name -- default %s] \n", filename);
         return -1;
     }
+
+    //imshow("Originial", src);
+
+    // Mat morph = src.clone();
+    // for (int r = 1; r <= 2; r++) {
+    //     Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(2 * r + 1, 2 * r + 1));
+    //     morphologyEx(morph, morph, MORPH_CLOSE, kernel);
+    //     morphologyEx(morph, morph, MORPH_OPEN, kernel);
+    // }
+    // imshow("Smoothing", morph);
+
+    // // Mat shifted;
+    // // pyrMeanShiftFiltering(src, shifted, 21, 51);
+    // // imshow("Mean shifting", shifted);
+
+    // Mat gray;
+    // cvtColor(morph, gray, COLOR_BGR2GRAY);
+
+    // /* take morphological gradient */
+    // Mat mgrad;
+    // Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(3, 3));
+    // morphologyEx(morph, mgrad, MORPH_GRADIENT, kernel);
+    // imshow("Grad", mgrad);
+    // Mat ch[3], merged;
+    // /* split the gradient image into channels */
+    // split(mgrad, ch);
+    // /* apply Otsu threshold to each channel */
+    // threshold(ch[0], ch[0], 0, 255, THRESH_BINARY | THRESH_OTSU);
+    // threshold(ch[1], ch[1], 0, 255, THRESH_BINARY | THRESH_OTSU);
+    // threshold(ch[2], ch[2], 0, 255, THRESH_BINARY | THRESH_OTSU);
+    // /* merge the channels */
+    // merge(ch, 3, merged);
+    // imshow("Merged", merged);
+    // Mat gray, binary;
+    // cvtColor(merged, gray, COLOR_BGR2GRAY);
+    // threshold(gray, binary, 1, 255, THRESH_BINARY);
+    // imshow("Binary", binary);
+
+    // morphologyEx(binary, binary, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(5,5)));
+    // imshow("MorphologyEx CLOSE", binary);
+
+    // waitKey();
     Mat gray;
     cvtColor(src, gray, COLOR_BGR2GRAY);
-    GaussianBlur(gray, gray, Size(5, 5), 20);
+    GaussianBlur(gray, gray, Size(5, 5), 20, 20);
+    // medianBlur(gray, gray, 5);
+    //imshow("Gaussian Blur", gray);
+
+    // Mat thresh;
+    // adaptiveThreshold(gray, thresh, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 11, 1);
+    // imshow("Adaptive Threshold", thresh);
+    // Mat morph;
+    // morphologyEx(thresh, morph, MORPH_CLOSE, getStructuringElement(MORPH_ELLIPSE, Size(7, 7)));
+    // imshow("Morphology CLOSE", morph);
+
+    // waitKey();
+
+    // vector<vector<Point>> contours;
+    // vector<Vec4i> hierarchy;
+    // findContours(morph, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+    // Mat filled = Mat::zeros(src.rows, src.cols, CV_8UC1);
+    // drawContours(filled, contours, -1, Scalar(255), -1);
+    // imshow("Contour filling", filled);
+
+    // // TO DO LATER
+    // Mat dist;
+    // distanceTransform(filled, dist, DIST_L2, 3);
+    // normalize(dist, dist, 0.0, 1.0, NORM_MINMAX);
+    // imshow("Distance", dist);
 
     vector<Vec3f> circles;
-    HoughCircles(gray, circles, HOUGH_GRADIENT, 1, gray.cols / 80, 100, 30, gray.cols / 80,
-                 gray.cols / 16);
+    HoughCircles(gray, circles, HOUGH_GRADIENT, 1, src.cols / 55, 80, 38, src.cols / 80,
+                 src.cols / 16);
 
+    float min = circles[0][2], max;
     Mat radii(circles.size(), 1, CV_32F), coin_labels, centers;
-    for (int i = 0; i < circles.size(); i++) {
+    radii.at<float>(0) = circles[0][2];
+    for (int i = 1; i < circles.size(); i++) {
         radii.at<float>(i) = circles[i][2];
+        if (circles[i][2] < min)
+            min = circles[i][2];
+        else if (circles[i][2] > max)
+            max = circles[i][2];
     }
-    kmeans(radii, 3, coin_labels,
+    int k = max / min > 2.2 ? 4 : 3;
+    kmeans(radii, k, coin_labels,
            TermCriteria(TermCriteria::MAX_ITER | TermCriteria::EPS, 10, .01), 10,
            KMEANS_PP_CENTERS, centers);
 
-    for (int i = 0; i < coin_labels.rows; i++) {
-        cout << coin_labels.at<int>(i) << endl;
+    int k_rad_index[3];
+    map<float, int> k_rad_sorted;
+    // cout << coin_labels.total() << endl;
+    for (int i = 0; i < k; i++) {
+        k_rad_index[i] =
+            find(coin_labels.ptr<int>(0), coin_labels.ptr<int>(coin_labels.total()), i) -
+            coin_labels.ptr<int>(0);
+        k_rad_sorted[radii.at<float>(k_rad_index[i])] = i;
+        // printf("Index: %d, radius: %f\n", k_rad_index[i], radii.at<float>(k_rad_index[i]));
     }
+    // for (auto t : k_rad_sorted) {
+    //     printf("K: %d, radius: %.2f\n", t.second, t.first);
+    // }
+
+
     float total = 0.0f;
     for (int i = 0; i < circles.size(); i++) {
         Point center = Point(circles[i][0], circles[i][1]);
         circle(src, center, 1, Scalar(0, 100, 100), 3, LINE_AA);
         Scalar clr;
-        switch (coin_labels.at<int>(i)) {
-            case 0:
-                clr = Scalar(0, 0, 255);
-                total += 0.01f;
-                break;
-            case 1:
-                clr = Scalar(0, 255, 255);
-                total += 0.05f;
-                break;
-            case 2:
-                clr = Scalar(255, 0, 255);
-                total += 0.25f;
-                break;
-            case 3:
-                clr = Scalar(0, 0, 0);
-                total += 0.50f;
+        map<float, int>::iterator iter = k_rad_sorted.begin();
+        if (coin_labels.at<int>(i) == iter++->second) {
+            clr = Scalar(0, 0, 255);
+            total += 0.01f;
+        } else if (coin_labels.at<int>(i) == iter++->second) {
+            clr = Scalar(0, 255, 255);
+            total += 0.05f;
+        } else if (coin_labels.at<int>(i) == iter++->second) {
+            clr = Scalar(255, 0, 255);
+            total += 0.25f;
+        } else if (coin_labels.at<int>(i) == iter++->second) {
+            clr = Scalar(0, 0, 0);
+            total += 0.50f;
         }
-        printf("Total: %.2f\n", total);
+        // printf("Total: %.2f\n", total);
         circle(src, center, circles[i][2], clr, 3, LINE_AA);
     }
-    printf("Total: %.2f", total);
+
+    printf("Total: %.2f\n", total);
+    imwrite("detected_coins.jpg", src);
     // Show results
-    imshow("Canny Circles", gray);
     imshow("Detected Circles", src);
     // Wait and Exit
     waitKey();
---
+++ lab07/platonic.cpp
@@ -0,0 +1,136 @@
+#define _USE_MATH_DEFINES
+#include <fstream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+struct point_comp {
+    bool operator()(const pair<int, int>& a, const pair<int, int>& b) const {
+        return less_comp(minmax(a.first, a.second), minmax(b.first, b.second));
+    }
+    less<std::pair<int, int>> less_comp;
+};
+
+double inline distance(cv::Point3f a, cv::Point3f b) {
+    return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) + (a.z - b.z) * (a.z - b.z));
+}
+
+void gluPerspective(const float& fov, const float& aspectRatio, const float& n, const float& f,
+                    float& t, float& b, float& l, float& r) {
+    float scale = tan(fov * 0.5 * M_PI / 180) * n;
+    r = aspectRatio * scale, l = -r;
+    t = scale, b = -t;
+}
+
+void glFrustum(const float& t, const float& b, const float& l, const float& r, const float& n,
+               const float& f, cv::Mat& M) {
+    M = (cv::Mat1f(4, 4) << 2 * n / (r - l), 0, 0, 0,                                     //
+         0, 2 * n / (t - b), 0, 0,                                                        //
+         (r + l) / (r - l), (t + b) / (t - b), -(f + n) / (f - n), -2 * f * n / (f - n),  //
+         0, 0, -1, 0);
+}
+
+void multPointMatrix(const cv::Point3f& in, cv::Point3f& out, const cv::Mat& M) {
+    cv::Mat temp = (cv::Mat1f(4, 1) << in.x, in.y, in.z, 1);
+    temp = M * temp;
+    out = cv::Point3f(temp.at<float>(0, 0), temp.at<float>(1, 0), temp.at<float>(2, 0));
+    if (temp.at<float>(0, 3) != 1) {
+        out.x /= temp.at<float>(3, 0);
+        out.y /= temp.at<float>(3, 0);
+        out.z /= temp.at<float>(3, 0);
+    }
+    // cout << temp.at<float>(3, 0) << " " << out << endl;
+}
+
+int main(int argc, char** argv) {
+    cout << "Input name of platonic solid: ";
+    string name;
+    cin >> name;
+
+    ifstream infile(name + ".txt", ios::in);
+
+    vector<cv::Point3f> points;
+    double ix, iy, iz;
+    while (infile >> ix >> iy >> iz) points.emplace_back(cv::Point3f(ix, iy, iz));
+
+    double mindist = INFINITY;
+    for (int i = 1; i < points.size(); ++i) {
+        mindist = min(mindist, distance(points[0], points[i]));
+        // cout << mindist << endl;
+    }
+
+    set<pair<int, int>, point_comp> edges;
+    for (int i = 0; i < points.size(); ++i)
+        for (int j = 0; j < points.size(); ++j)
+            if (distance(points[i], points[j]) - mindist < 0.0001)
+                edges.emplace(pair<int, int>(i, j));
+    // for (auto a : edges) {
+    //     cout << a.first << " " << a.second << endl;
+    //     cout << points[a.first] << " " << points[a.second] << endl;
+    // }
+
+    cv::Mat Mproj, worldCamera = (cv::Mat1f(4, 4) << 1, 0, 0, 0,  //
+                                  0, 1, 0, 0,                     //
+                                  0, 0, -1, -4,                   //
+                                  0, 0, 0, 1                      //
+                   );
+
+    int width = 512, height = 512;
+    cv::Size size(width, height);
+    cv::VideoWriter outputVideo("output.avi", cv::VideoWriter::fourcc('M','J','P','G'), 20,
+                                size);
+
+    if (!outputVideo.isOpened()) {
+        cout << "Could not open the output video for write: " << "output.avi" << endl;
+        return -1;
+    }
+
+    cv::Scalar color(255, 0, 0);
+
+    float fov = 90;
+    float near = 0.1;
+    float far = 100;
+    float aspectRatio = width / (float)height;
+    float b, t, l, r;
+    gluPerspective(fov, aspectRatio, near, far, t, b, l, r);
+    glFrustum(t, b, l, r, near, far, Mproj);
+
+
+    cout << "Generating video..." << endl;
+    for (float angle = 0; angle < 30; angle+=0.1) {
+        cv::Mat frame(size, CV_8UC3);
+        vector<cv::Point2i> projected(points.size());
+        for (int i = 0; i < points.size(); i++) {
+            cv::Point3f vertCam, projVert;
+            multPointMatrix(points[i], vertCam, worldCamera);
+            multPointMatrix(vertCam, projVert, Mproj);
+            projected[i] =
+                cv::Point2i(min(width - 1, (int)((projVert.x + 1) * 0.5 * width)),
+                            min(height - 1, (int)((1 - (projVert.y + 1) * 0.5) * height)));
+        }
+
+        for (auto elem : edges) {
+            cv::Point3f vertCamera, projVert;
+            // cout << projected[elem.first] << projected[elem.second] << endl;
+            cv::line(frame, projected[elem.first], projected[elem.second], color, 3);
+        }
+
+        float rad = angle * M_PI / 180.0;
+        for (auto& vert : points) {
+            float old = vert.x;
+            vert.x = vert.x * cos(rad) - vert.z * sin(rad);
+            vert.z = vert.z * cos(rad) + old * sin(rad);
+        }
+
+        // cv::imshow("Hello", frame);
+        // cv::waitKey(0);
+
+        outputVideo << frame;
+    }
+
+    outputVideo.release();
+
+    cout << "Done!" << endl;
+
+    return 0;
+}
